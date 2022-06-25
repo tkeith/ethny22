ba633a40b2421d1f4df3d2d0c753ba4611be0911
Lea <lmtejedor@gmail.com>
2022-06-25 15:48:07 -0400

    embed trees

--- src/next/pages/explore.js
+++ src/next/pages/explore.js
@@ -3,7 +3,9 @@ import Frame from '../components/Frame.js';
 export default function Explore() {
   return <>
     <Frame title='Explore'>
-      <p>Explore UI goes here</p>
+      <div class="glitch-embed-wrap">
+        <iframe src="https://glitch.com/embed/#!/embed/generative-trees?path=index.html&previewSize=100"></iframe>
+      </div>
     </Frame>
   </>
 }
